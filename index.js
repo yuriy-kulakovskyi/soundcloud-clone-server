989b293330793893c86d884d241a85f2c5a6cbe0
yuriy-kulakovskyi <yurakulak42@gmail.com>
2024-07-23 14:11:49 +0300

    fix

--- index.js
+++ index.js
@@ -35,4 +35,7 @@ app.listen(port, (req, res) => {
   console.log(`Server is running on port: ${port}`);
 });
 
-mongoose.connect(uri).then(() => console.log("Mongo connection established")).catch((err) => console.log("Mongo connection failed: ", err.message))
+mongoose.connect('mongodb://localhost:27017/yourdbname', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => console.log("Mongo connection established")).catch((err) => console.log("Mongo connection failed: ", err.message))
