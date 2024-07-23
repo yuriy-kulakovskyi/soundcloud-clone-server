c693b724a4fbc9f2ca91f40e9ecbb591786dc4e9
yuriy-kulakovskyi <yurakulak42@gmail.com>
2024-07-23 17:44:25 +0300

    allowed all domains

--- index.js
+++ index.js
@@ -10,7 +10,13 @@ const app = express();
 require("dotenv").config();
 
 app.use(express.json());
-app.use(cors());
+
+app.use(cors({
+  origin: '*',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
 app.use("/api/users", userRoute);
 app.use("/api/songs", songRoute);
 
